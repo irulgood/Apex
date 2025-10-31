5761faa2fc161ef86b37b4b0c194abe3cef98403
irulgood <153479041+irulgood@users.noreply.github.com>
2025-10-31 11:10:57 +0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -4,15 +4,15 @@
 # BY KHOIRUL AMIR
 
 # === CONFIG TELEGRAM ===
-BOT_TOKEN="8379519489:AAE6YLcEi9ilkkQmtXHZWM_WYhd4m2mDEJw"  # Ganti token kamu
-CHAT_ID="5376506914"                                         # Ganti chat ID kamu
+BOT_TOKEN="8379519489:AAE6YLcEi9ilkkQmtXHZWM_WYhd4m2mDEJw"
+CHAT_ID="5376506914"
+export TIME="10"
 
 # === INFO VPS ===
 IP=$(curl -sS ipv4.icanhazip.com)
 domain=$(cat /etc/xray/domain 2>/dev/null)
 date=$(date +"%Y-%m-%d")
 
-# === PROSES BACKUP ===
 echo "Proses backup sedang berlangsung..."
 rm -rf /root/backup
 mkdir -p /root/backup
@@ -30,21 +30,30 @@ cd /root
 zip -r $IP-$date.zip backup > /dev/null 2>&1
 
 # === UPLOAD KE GOOGLE DRIVE ===
-rclone copy /root/$IP-$date.zip dr:backup/ --progress
+rclone copy /root/$IP-$date.zip dr:backup/ > /dev/null 2>&1
 url=$(rclone link dr:backup/$IP-$date.zip)
-id=$(echo "$url" | grep -oP 'id=\K[^&]+')
+id=$(echo $url | grep '^https' | cut -d'=' -f2)
 link="https://drive.google.com/u/4/uc?id=${id}&export=download"
 
 # === KIRIM NOTIF TELEGRAM ===
-TEXT=$(echo -e "<code>◇━━━━━━━━━━━━━━◇</code>\n<b>⚠️ BACKUP OTOMATIS ⚠️</b>\n<b>Detail Backup VPS</b>\n<code>◇━━━━━━━━━━━━━━◇</code>\n<b>IP VPS :</b> <code>${IP}</code>\n<b>DOMAIN :</b> <code>${domain}</code>\n<b>Tanggal :</b> <code>${date}</code>\n<code>◇━━━━━━━━━━━━━━◇</code>\n<b>Link Backup :</b> ${link}\n<code>◇━━━━━━━━━━━━━━◇</code>\n<code>Backup dibuat otomatis setiap hari.</code>\n<code>BY BOT : @Kamirr21</code>")
+TEXT="
+<code>◇━━━━━━━━━━━━━━◇</code>
+<b>⚠️ BACKUP OTOMATIS ⚠️</b>
+<b>Detail Backup VPS</b>
+<code>◇━━━━━━━━━━━━━━◇</code>
+<b>IP VPS :</b> <code>${IP}</code>
+<b>DOMAIN :</b> <code>${domain}</code>
+<b>Tanggal :</b> <code>${date}</code>
+<code>◇━━━━━━━━━━━━━━◇</code>
+<b>Link Backup :</b> ${link}
+<code>◇━━━━━━━━━━━━━━◇</code>
+<code>Backup dibuat otomatis setiap hari.</code>
+<code>BY BOT : @Kamirr21</code>
+"
 
-curl -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" \
--d chat_id="$CHAT_ID" \
--d "parse_mode=HTML" \
--d "disable_web_page_preview=true" \
--d text="$TEXT"
+curl -s --max-time $TIME -d "chat_id=$CHAT_ID&disable_web_page_preview=1&text=$TEXT&parse_mode=html" \
+https://api.telegram.org/bot$BOT_TOKEN/sendMessage >/dev/null
 
-# === HAPUS FILE SEMENTARA ===
 rm -rf /root/backup
 rm -f /root/$IP-$date.zip
 
