7d9ce3d888554cb2add0be21b90ed3aec8f67727
irulgood <153479041+irulgood@users.noreply.github.com>
2025-11-05 10:12:14 +0700

    Update insttal-backup.sh

--- insttal-backup.sh
+++ insttal-backup.sh
@@ -24,14 +24,14 @@ fi
 chmod +x "$TARGET"
 
 # Tambahkan cronjob jika belum ada
-CRON_JOB="0 23 * * * /bin/bash $TARGET"
+CRON_JOB="0 21 * * * /bin/bash $TARGET"
 
 # Cek apakah cron sudah ada
 if crontab -l 2>/dev/null | grep -q "$TARGET"; then
     echo "ℹ️  Cronjob sudah ada, gak perlu ditambah lagi."
 else
     (crontab -l 2>/dev/null; echo "$CRON_JOB") | crontab -
-    echo "🕒 Cronjob berhasil ditambahkan (backup tiap jam 23:00)."
+    echo "🕒 Cronjob berhasil ditambahkan (backup tiap jam 21:00)."
 fi
 
 echo "🚀 Setup selesai! Backup otomatis aktif tiap malam."
